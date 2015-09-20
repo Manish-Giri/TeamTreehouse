0a446e97c8ce7471e37f3507016bdbe0d1a16fa4
Manish-Giri <manish.giri.me@gmail.com>
2015-09-19 22:06:54 -0400

    View Controller header file from the Data collection challenge

---
+++ iOS-Development/Simple-iPhone-App/Challenges/Data-Collection/ViewController.h
@@ -0,0 +1,8 @@
+#import "UIViewController.h"
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) NSString *shoppingCart;
+@property (nonatomic, strong) NSArray *shoppingList;
+
+@end
